402ececceb509bd6a3192a5cbdf81f9a086710cb
lone <fanmenglong@abtnetworks.com>
2023-08-28 18:15:47 +0800

    init commit

---
+++ elementary/people/the_family.py
@@ -0,0 +1,42 @@
+key_words = """
+    relatives relations
+    children
+    husband father
+    wife mother
+    parents
+    daughter son
+    sister brother
+    grandfather grandmother
+    grandparents
+    granddaughter grandson
+    uncle aunts
+    niece nephew
+    cousins
+"""
+
+exercises = """
+1.1
+    brother
+    aunt
+    uncle
+    grandmother
+    grandfather
+    nephew
+    niece
+    mother
+    wife
+    cousin
+1.2
+    aunt
+    only child
+    father
+    wife
+    mother
+    granddaughter
+    grandson
+    grandparents
+1.3
+    Yes, I have a brother.
+    No, I haven't.
+    ......
+"""
